d65714af07972d95154343fc30908265fd4d0672
Slav-0N <SOVILGO@gmail.com>
2023-12-10 20:01:17 +0200

    пагінація готова

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,10 +2,13 @@ const { Contact } = require("../../models");
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const result = await Contact.find({ owner: _id }).populate(
-    "owner",
-    "_id email"
-  );
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find({ owner: _id }, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id email");
   res.json(result);
 };
 
