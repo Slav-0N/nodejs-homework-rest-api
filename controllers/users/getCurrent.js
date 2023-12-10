0c0ac81049f9bb3c0c5ca37132979c195aa19d3f
Slav-0N <SOVILGO@gmail.com>
2023-12-10 23:35:18 +0200

    getCurrent доробив

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,6 +1,13 @@
-// const { User } = require("../../models");
 const getCurrent = async (req, res) => {
-  console.log(req.user);
+  const { email, subscription } = req.user;
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      email,
+      subscription,
+    },
+  });
 };
 
 module.exports = getCurrent;
